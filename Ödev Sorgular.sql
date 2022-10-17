d6222846d0aafff736a23bbb2fe55a91a2693c70
ilker yüce <109898238+ilkeryuce1@users.noreply.github.com>
2022-10-18 00:10:53 +0300

    Add files via upload

---
+++ Ödev Sorgular.sql
@@ -0,0 +1,22 @@
+--select SUM(product_price) from tbl_Product
+
+--select COUNT(product_quantity) from tbl_Product where product_quantity<15
+
+--select SUM(Price) as'Total Ciro' from tbl_Customer
+
+--select AVG(price) from tbl_Customer
+
+--select MAX(product_quantity) as 'En Yüksek Ürün Fiyatý ' from tbl_Product
+
+--select max(ord.product_price) from tbl_Product ord
+
+--select MIN(Price) from tbl_Customer where  Customer_name_Surname between 'd' and 'k'
+
+--select min(price) from tbl_Customer od where Price>120
+
+
+--select *from tbl_Customer ord where ord.hall_id in (2,4,5,7)
+
+
+--select MAX(Price) from tbl_Customer ord where spersonal_id in(1,2)
+
