0a030e83d2b0ab4e9ba5fd222ed38cef17b8da35
ilker yüce <109898238+ilkeryuce1@users.noreply.github.com>
2022-11-10 23:36:11 +0300

    Add files via upload

---
+++ Odevler/Ogrenci_Kayıt_Otomasyonu/Ogrenci_Kayıt_Otomasyonu/Model1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Ogrenci_Kayıt_Otomasyonu
+{
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model11")
+        {
+        }
+
+        public virtual DbSet<Ogrenci> Ogrencis { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ogrenci>()
+                .Property(e => e.Durum)
+                .IsFixedLength();
+        }
+    }
+}
